138601463b214d3972e981bc010c121345f1aa0e
Radu Dumitru <radu.dumitru05@gmail.com>
2023-03-20 14:36:30 +0000

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-material-dynamic-theme';
+export class AppComponent implements OnInit {
+  links: string[] = [];
+  font = new FormControl('Montserrat');
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  ngOnInit() {
+    for(let i = 1; i <= 100; i++) {
+      this.links.push('Link ' + i);
+    }
+
+    this.font.valueChanges.subscribe(font => {
+      this.document.documentElement.style.setProperty('--font-family', font);
+    });
+  }
 }
